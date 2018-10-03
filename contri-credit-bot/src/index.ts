5624f72cf46018285756c9df2c60b389c8c402ff
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-10-02 17:05:41 -0700

    Modifying the main index file to now handle all events (though there are problems where it's only picking up the push event.. not sure why. Also adding some test command for the push even and the necessary data for that commad"

--- contri-credit-bot/src/index.ts
+++ contri-credit-bot/src/index.ts
@@ -3,9 +3,19 @@ import { Application } from 'probot'
 export = (app: Application) => {
   // Your code here
   app.log('Yay, the app was loaded!')
-  app.on('push', async context => {
-    app.log(context)
+  // app.on(['push','issue.opened'], async context => {
+  app.on(`*`, async context => {
+    console.log(`We should grab the person and the thing -- ${context.name} -- and add their name to the contributors file, .github/contri-credit.md`);
+    if (context.name === 'push'){
+      console.log(`${context.payload.pusher.name} just did a ${context.name}`);
+    }
+  });
+
+  /* Using wildcard event to catch anything our app already listens to
+   app.on(`*`, async context => {
+    context.log({event: context.event, action: context.payload.action})
   })
+  */
 
   // For more information on building apps:
   // https://probot.github.io/docs/
