7d876a53cd01fc000ad04ad2506d6f5025926d10
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-10-08 17:08:26 -0700

    Removing Axios package and calls from bot code. Probot API is able to make the necessary changes to write to the repo. Currently this is (over)writing the .github/contri-credit.md

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Application } from 'probot'
-import axios from 'axios'
 
 export = (app: Application) => {
-  // Your code here
   app.log('Yay, the app was loaded!')
-  // app.on(['push','issue.opened'], async context => {
+ /* Using wildcard event to catch anything our app already listens to
+  app.on(`*`, async context => {
+    context.log({ event: context.event, action: context.payload.action })
+  })
+  */
   app.on(`*`, async context => {
     console.log(`We should grab the person and the thing -- ${context.name} -- and add their name to the contributors file, .github/contri-credit.md`);
     if (context.name === 'push') {
@@ -17,53 +19,27 @@ export = (app: Application) => {
       console.log(context)
     }
 
-    // throwing these into comments until i get things working
-    let options = {
-      baseURL: 'https://api.github.com/',
-      method: 'PUT',
-      url: `/repos/rainrivas/probot-test-playground/contents/.github/contri-credit.md`,
-      headers: {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
-        'User-Agent': 'the-after-hours',
-        'Authorization': `${process.env.WEBHOOK_SECRET}`
-      },
-      body:
-      {
-        message: 'Creating the contrib file',
-        content: 'c2FtcGxlIHRlc3QgZmlsZQ==',
-        commiter: { name: 'Efrain \'Rain\' Rivas', email: 'rainrivas@gmail.com' },
-        branch: 'test-branch2'
-      },
-      json: true
-    };
+    // Updating file based on info from github octokit
+      // https://octokit.github.io/rest.js/#api-Repos-updateFile
+    let owner = 'rainrivas',
+      branch = 'test-branch2',
+      // committer = { name: 'Efrain \'Rain\' Rivas', email: 'rainrivas@gmail.com' },
+      fileBody = '\nAdding new contribs to the magic file',//'c2FtcGxlIHRlc3QgZmlsZQ==',
+      message = 'Modifying contrib file',
+      path = '.github/contri-credit.md',
+      repo = 'probot-test-playground',
+      sha = 'ac85ae3242b0e7e1c9d69b6d33c1d382816f9dc9'; // currently this is hard coded, we need to make a GET action to figure out if this file exists to grab sha
 
+      // file contents need to be encoding so doing that here
+    let buff = new Buffer(fileBody);
+    let content = buff.toString('base64');
 
-    /* 
-    Run the GET first and see if the file exists
-    If file not found we should create it (PUT)
-    If it does, we should update it (PUT but with the "SHA" param)
-    */
-    // This doesn't work yet so comments
-    axios(options)
-      .catch(function (error) {
-        console.info(`we goofed`);
-        console.error(error);
-      })
-      .then(function (response) {
-        console.info('Reponse received.');
-        console.info(response);
-      });
+    // Need to make the call to figure out if the file already exists and if it does we need to grab it's sha so we can pass it to the "udpate" file command.
 
-    // https://developer.github.com/v3/repos/contents/#update-a-file
-    // PUT /repos/:owner/:repo/contents/:path
-    // https://api.github.com
+    // await context.github.repos.createFile({ owner, repo, path, message, content, branch, committer }); // create the file
+    const res = await context.github.repos.updateFile({ owner, repo, path, message, content, sha, branch }); // update a created file
 
-    /* Using wildcard event to catch anything our app already listens to
-     app.on(`*`, async context => {
-      context.log({event: context.event, action: context.payload.action})
-    })
-    */
+    return console.info(res)
 
     // For more information on building apps:
     // https://probot.github.io/docs/
