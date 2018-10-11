3e5856f7da7713d717b7a931f8295748cee8a5d0
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-10-10 22:41:38 -0700

    Expanding logic for creating and updating file. Also removing alot of the noise from old comments. Still have plenty to sort out since the formatting of the contrib file isn't up to par and the way we count contributions is currently missing. Adding todo block to hopeful remind us of this work

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Application } from 'probot'
 
+/* Majority of this is using the octokit to interact with GH API 
+* https://octokit.github.io/rest.js/
+*/
 export = (app: Application) => {
+  let updateMessage = ''
   app.log('Yay, the app was loaded!')
   /* Using wildcard event to catch anything our app already listens to
    app.on(`*`, async context => {
@@ -12,16 +16,18 @@ export = (app: Application) => {
     if (context.name === 'push') {
       console.debug(JSON.stringify(context))
       console.debug(`${context.payload.pusher.name} just did a ${context.name}`); // We need to store this information for when we update the contents of contrib file
+      updateMessage = `${context.payload.pusher.name} just did a ${context.name}`
     }
     else if (context.name === 'create') {
       console.debug(JSON.stringify(context))
       console.debug(`${context.payload.sender.login} just did a ${context.name} AKA Pull Request`); // We need to store this information for when we update the contents of contrib file
+      updateMessage = `${context.payload.sender.login} just did a ${context.name} AKA Pull Request`
     }
     else {
-      console.debug(context)
+      console.debug(JSON.stringify(context))
     }
     /* 
-    * Important Context Info
+    * Important Context Info 
     * context.payload.ref == branch <-- lets us find the contrib file refrence
     * context.payload.repository.name == repo name <-- Def Need for calls
     * context.payload.repository.owner.name == owner name (rainrivas) <-- need for calls I think
@@ -31,22 +37,22 @@ export = (app: Application) => {
     let owner = context.payload.repository.owner.login,
       repo = context.payload.repository.name,
       // todo: test writing to files the bot should not be able to access
-      path = '.github/contri-credit.md', 
+      path = '.github/contri-credit.md',
       ref = context.payload.ref
+    let branch = ref // Have to do this becaues the createFile method doens't like having branch be set to 'ref'
 
     // this returns an error if no file found : ERROR event: {"message":"Not Found","documentation_url":"https://developer.github.com/v3/repos/contents/#get-contents"}
     try {
-      const result = await context.github.repos.getContent({ owner, repo, path, ref })
-      console.debug(result.data.sha) // the sha of the found file
-      // now we do update work with this, which we should probably also wrap into try-catch.. iunno.
-      // const res = await context.github.repos.updateFile({ owner, repo, path, message, content, sha, branch }); // update a created file, branch (opt),
+      const getFile = await context.github.repos.getContent({ owner, repo, path, ref })
+      console.debug(JSON.stringify(getFile)) // the sha of the found file
+      var content64 = getFile.data.content,
+      sha = getFile.data.sha
     } catch (error) {
       // Handle error when we can't get file
       console.error(error);
       if (error.code === 404) {
         console.debug('404 means it is time to make it happen capn');
-        let branch = ref, // Have to do this becaues the createFile method doens't like having branch be set to 'ref'
-        fileBody = '\nAdding new contribs to the magic file', // eventually add the info from app.on(*) if-else blocks
+        let fileBody = '\nAdding new contribs to the magic file', // eventually add the info from app.on(*) if-else blocks
           message = 'Creating contrib file' // This is the commit message we'll be seeing
         //  file contents need to be encoding so doing that here
         let buff = new Buffer(fileBody);
@@ -56,40 +62,21 @@ export = (app: Application) => {
         console.debug(createContrib.status) // should be a 201
       }
     }
-
-    //   let owner = 'rainrivas',
-    //     branch = 'test-branch2',
-    //     // committer = { name: 'Efrain \'Rain\' Rivas', email: 'rainrivas@gmail.com' },
-    //     // fileBody = '\nAdding new contribs to the magic file',//'c2FtcGxlIHRlc3QgZmlsZQ==',
-    //     // message = 'Modifying contrib file',
-    //     path = '.github/contri-credit.md',
-    //     repo = 'probot-test-playground'
-    //     // sha = 'ac85ae3242b0e7e1c9d69b6d33c1d382816f9dc9'; // currently this is hard coded, we need to make a GET action to figure out if this file exists to grab sha
-
-    //   /* Get contents of the file
-    //   * https://octokit.github.io/rest.js/#api-Repos-getContent
-    //   * owner	string
-    //   * repo	string
-    //   * path	string The content path.
-    //   * ref string (optional) The name of the commit/branch/tag. Default value: the repository’s default branch (usually `master`)
-    //   */
-    //  let ref = branch;
-    //   const result = await context.github.repos.getContent({ owner, repo, path, ref })
-
-    //   // Updating file based on info from github octokit
-    //   // https://octokit.github.io/rest.js/#api-Repos-updateFile
-
-    //   // file contents need to be encoding so doing that here
-    //   // let buff = new Buffer(fileBody);
-    //   // let content = buff.toString('base64');
-
-    //   // Need to make the call to figure out if the file already exists and if it does we need to grab it's sha so we can pass it to the "udpate" file command.
-
-    //   // await context.github.repos.createFile({ owner, repo, path, message, content, branch, committer }); // create the file
-    //   // const res = await context.github.repos.updateFile({ owner, repo, path, message, content, sha, branch }); // update a created file
-
-
-    //   return console.debug(result)
+    if (sha) { // if we do have a sha, lets update this bad boy
+      try {
+        // now we do update work with this, which we should probably also wrap into try-catch.. iunno.
+        let oldContent = new Buffer(content64,'base64').toString('ascii')
+        let updateBuff = new Buffer(oldContent + '<br>' + updateMessage)
+        let content = updateBuff.toString('base64'),
+          message = 'Updating contrib file with new user contribution'
+        // todo: When updating the file, we should make a call to the contributors part of the context so we can add totals up
+        // body: Use the contributors call -- https://octokit.github.io/rest.js/#api-Repos-getContributors -- const result = await octokit.repos.getContributors({owner, repo, anon, per_page, page})
+        const updateFile = await context.github.repos.updateFile({ owner, repo, path, message, content, sha, branch }); // update a created file, branch (opt),
+        console.debug(updateFile.status)
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
     //   // For more information on building apps:
     //   // https://probot.github.io/docs/
